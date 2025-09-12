8bc18312894d972af41350473cdbbebe04084274
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-09-11 23:41:36 -0300

    criando ambiente e adicionando dependencias

---
+++ nubank/src/main/java/desafio_nubank/NubankApplication.java
@@ -0,0 +1,16 @@
+package desafio_nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankApplication {
+
+	public static void main(String[] args) {
+        System.out.println("xd");
+		SpringApplication.run(NubankApplication.class, args);
+        System.out.println("oi");
+
+	}
+
+}
